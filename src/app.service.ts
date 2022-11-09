5c254f7bfe9b42bd9dc881af60378edadf985ef5
Daichi Nishida <verhichi@hotmail.com>
2022-11-10 00:23:41 +0900

    chore: smaple code for creating new file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createFile(path: string, content: string) {
+    console.log(path);
+    try {
+      fs.writeFileSync(`../../${path}`, content);
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
